d5b0bb601232cb0176ca7b8efb06141479c3fa0c
mac_knight141 <123813453+isharax9@users.noreply.github.com>
2024-12-11 02:29:06 +0530

    design created ✅

--- app/src/main/java/lk/macna/app3/SignIn.java
+++ app/src/main/java/lk/macna/app3/SignIn.java
@@ -1,6 +1,7 @@
 package lk.macna.app3;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +21,9 @@ public class SignIn extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+
+
     }
 }
